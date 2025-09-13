bd3959d3bdd332e2c04340eec4b0f90a254a4085
LTC-117 <lucastentoni@gmail.com>
2025-09-13 15:22:36 -0300

    Correcting indentation.

--- algorithms/binarytree.c
+++ algorithms/binarytree.c
@@ -1,58 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> 
+
+
 typedef struct Node Node;
+
+
 struct Node{
     int value;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 };
 
-Node* CreateNode(int value) {
-    Node* node=(Node*)malloc(sizeof(Node));
-    node->value=value;
+
+Node *node_create(int value) {
+    Node *node = (Node *)malloc(sizeof(Node));
+
+    node->value = value;
     node->left = NULL;
     node->right = NULL;
+
+    return node;
 }
-void AddValue(Node* node,int value){
-    if(value>node->value){
-        if(node->right) return AddValue(node->right,value);
-        node->right=CreateNode(value);
-    }else if(value<node->value){
-        if(node->left) return AddValue(node->left,value);
-        node->left=CreateNode(value);
+
+
+void node_add_value(Node *node,int value) {
+    if(value > node->value) {
+        if(node->right) {
+            return node_add_value(node->right, value);
+        }
+        node->right = node_create(value);
+    } else if(value < node->value) {
+        if(node->left) {
+            return node_add_value(node->left, value);
+        }
+        node->left = node_create(value);
     }
 }
-void Print(Node* node){
-    if(node->left) Print(node->left);
+
+
+void node_print(Node *node) {
+    if(node->left) node_print(node->left);
+
     printf("%d ",node->value);
-    if(node->right) Print(node->right);
+
+    if(node->right) node_print(node->right);
 }
-int findValue(Node* node,int value){
-    if( node->value > value ) {
-        if(node->left)return findValue(node->left,value);
-    }
-    else if( node->value < value ) {
-        if(node->right) return findValue(node->right,value);
-    }
-    else return 1;
+
+
+int node_find_value(Node *node,int value) {
+    if(node->value > value) {
+        if(node->left) return node_find_value(node->left, value);
+    } else if(node->value < value) {
+        if(node->right) return node_find_value(node->right, value);
+    } else return 1;
+
     return 0;
 }
-Node* CreateRandomTree(){
-    
+
+
+Node *node_new_tree() {
     int n = 5,i;
+
     srand(time(0));
-    Node* root=CreateNode(rand() % 100);
-    int deep= rand() % 50;
-    for(  i=0 ; i<deep; i++){
-        AddValue(root,rand() %  100);
+
+    Node *root = node_create(rand() % 100);
+    int deep = rand() % 50;
+
+    for(i = 0 ; i < deep; i++) {
+        node_add_value(root, rand() % 100);
     }
+
     return root;
 }
 
-int main(){
-    Node* x=CreateRandomTree();
-    AddValue(x,20);
-    Print(x);
+
+int main(void) {
+    Node *x = node_new_tree();
+
+    node_add_value(x, 20);
+    node_print(x);
+
     return 0;
 }
